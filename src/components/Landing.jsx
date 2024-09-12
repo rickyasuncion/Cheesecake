712f03881ea79fc93371e4c121a4f24084ae0eee
rickyasuncion <rickyasuncion@outlook.com>
2024-09-12 09:00:13 -0600

    fix trending movies and queried movies overlap

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -9,8 +9,9 @@ const MOVIE_DISPLAY_COUNT = 5;
 
 const Landing = () => {
   const [searchTerm, setSearchTerm] = useState("");
-  const [movies, setMovies] = useState([]);
-  const [tvShows, setTvShows] = useState([]);
+  const [queriedMovies, setQueriedMovies] = useState([]);
+  const [trendingMovies, setTrendingMovies] = useState([]);
+  const [trendingTvShows, setTrendingTvShows] = useState([]);
   const [currMoviesObj, setCurrMoviesObj] = useState({
     start: 0,
     end: MOVIE_DISPLAY_COUNT,
@@ -25,7 +26,7 @@ const Landing = () => {
     const fetchedMovies = await fetchData(
       `https://api.themoviedb.org/3/search/multi?query=${searchTerm}&include_adult=false&language=en-US&page=1&api_key=bbd89781c7835917a2decb4989b56470`
     );
-    setMovies(fetchedMovies);
+    setQueriedMovies(fetchedMovies);
   };
 
   useEffect(() => {
@@ -33,12 +34,12 @@ const Landing = () => {
       const popularMovies = await fetchData(
         `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&api_key=bbd89781c7835917a2decb4989b56470`
       );
-      setMovies(popularMovies);
+      setTrendingMovies(popularMovies);
 
-      const popularTvShows = await fetchData(
+      const populartrendingTvShows = await fetchData(
         `https://api.themoviedb.org/3/trending/tv/day?language=en-US&page=1&api_key=bbd89781c7835917a2decb4989b56470`
       );
-      setTvShows(popularTvShows.slice(0, 5));
+      setTrendingTvShows(populartrendingTvShows.slice(0, 5));
     }
     populateData();
   }, []);
@@ -66,7 +67,7 @@ const Landing = () => {
       <h1 className="text-2xl font-semibold mb-4 text-center">
         Welcome to Movie Recommendation
       </h1>
-      <p className="text-center">Search for your favorite movies below:</p>
+      <p className="text-center">Search for your favorite Movies below:</p>
       <form
         className="mt-2 flex w-fit items-center mx-auto"
         onSubmit={handleSearchSubmit}
@@ -82,18 +83,18 @@ const Landing = () => {
       </form>
 
       <div className="container">
-        {movies.length === 0 ? (  
-          <h2 className="text-center mt-10 text-lg">No movies to display</h2>
+        {queriedMovies.length === 0 ? (
+          <h2 className="text-center mt-10 text-lg">No Movies to display</h2>
         ) : (
           <div className="mt-10">
             <div className="flex gap-2 items-center justify-between">
               <h2 className="text-lg font-medium">
-                Recommended Popular Movies
+                Search Results...
               </h2>
               <div className="flex gap-2 items-center">
                 <span>
-                  {currMoviesObj.start} to {currMoviesObj.end} movies /{" "}
-                  {movies.length} movies
+                  {currMoviesObj.start} to {currMoviesObj.end} Movies /{" "}
+                  {queriedMovies.length} Movies
                 </span>
                 <div>
                   <Button
@@ -110,7 +111,7 @@ const Landing = () => {
                   </Button>
                   <Button
                     className="text-xs p-1"
-                    disabled={currMoviesObj.end >= movies.length}
+                    disabled={currMoviesObj.end >= queriedMovies.length}
                     onClick={() =>
                       setCurrMoviesObj((curr) => ({
                         start: curr.start + MOVIE_DISPLAY_COUNT,
@@ -125,7 +126,7 @@ const Landing = () => {
             </div>
 
             <div className="movie-list">
-              {[...movies]
+              {[...queriedMovies]
                 .slice(currMoviesObj.start, currMoviesObj.end)
                 .map((movie) => (
                   <MovieCard
@@ -146,11 +147,67 @@ const Landing = () => {
       <div className="container">
         <div className="mt-10">
           <div className="flex gap-2 items-center justify-between">
+            <h2 className="text-lg font-medium">Recommended Popular Movies</h2>
+            <div className="flex gap-2 items-center">
+              <span>
+                {trendingMovies.start} to {trendingMovies.end} Movies /{" "}
+                {trendingMovies.length} Movies
+              </span>
+              <div>
+                <Button
+                  className="text-xs p-1"
+                  disabled={trendingMovies.start <= 0}
+                  onClick={() =>
+                    setCurrMoviesObj((curr) => ({
+                      start: curr.start - MOVIE_DISPLAY_COUNT,
+                      end: curr.end - MOVIE_DISPLAY_COUNT,
+                    }))
+                  }
+                >
+                  <ChevronLeft />
+                </Button>
+                <Button
+                  className="text-xs p-1"
+                  disabled={currMoviesObj.end >= trendingMovies.length}
+                  onClick={() =>
+                    setCurrMoviesObj((curr) => ({
+                      start: curr.start + MOVIE_DISPLAY_COUNT,
+                      end: curr.end + MOVIE_DISPLAY_COUNT,
+                    }))
+                  }
+                >
+                  <ChevronRight />
+                </Button>
+              </div>
+            </div>
+          </div>
+
+          <div className="movie-list">
+            {[...trendingMovies]
+              .slice(currMoviesObj.start, currMoviesObj.end)
+              .map((movie) => (
+                <MovieCard
+                  key={movie.id}
+                  id={movie.id}
+                  imageUrl={movie.imageUrl}
+                  media_type={movie.media_type}
+                  title={movie.title}
+                  name={movie.name}
+                  poster_path={movie.poster_path}
+                />
+              ))}
+          </div>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="mt-10">
+          <div className="flex gap-2 items-center justify-between">
             <h2 className="text-lg font-medium">Popular TV Shows</h2>
           </div>
 
           <div className="movie-list">
-            {tvShows.map((movie) => (
+            {trendingTvShows.map((movie) => (
               <MovieCard
                 key={movie.id}
                 id={movie.id}
