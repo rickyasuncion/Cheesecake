ba05b153d87edb7faeb8d477e732e1feac73cedd
willhui1204 <siukee.hui@edu.sait.ca>
2024-09-13 14:41:09 -0600

    error fixed

--- src/components/LandingPage/LandingSearchForm.jsx
+++ src/components/LandingPage/LandingSearchForm.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Input from "../ui/input";
-import Button from "../ui/button";
+import {Button} from "../ui/button";
 // import Input from "../ui/input";
 // import {Button} from "./ui/button";
 
