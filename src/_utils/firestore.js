a5cb22f47d9f6b46b4ed281bde766c3a496e9efe
rickyasuncion <ricky.asuncion@edu.sait.ca>
2024-09-23 14:46:00 -0600

    Update firestore.js
    create createReview function

--- src/_utils/firestore.js
+++ src/_utils/firestore.js
@@ -4,23 +4,61 @@ import { doc, setDoc, getFirestore, updateDoc } from "firebase/firestore";
 const auth = getAuth();
 const db = getFirestore();
 
-async function createUserWithProfile(email, password, profileData) {
+async function createUserWithProfile(email, password) {
   try {
     const userCredential = await createUserWithEmailAndPassword(
       auth,
       email,
-      password,
+      password
     );
     const user = userCredential.user;
-    
+
     const userDocRef = doc(db, "users", user.uid);
     await setDoc(userDocRef, {
-        uid: user.uid,
-        email: user.email,
-        displayName: user.displayName,
-        favourites: [{}]
+      uid: user.uid,
+      email: user.email,
+      displayName: user.displayName,
+      favourites: [{}],
+      reviews: [{}],
     });
 
+    console.log("User created successfully!");
+  } catch (error) {
+    console.error("Error creating user:", error);
+  }
+}
+
+async function createReview(content, media_type, media_id, uid, displayName) {
+  try {
+    const media = {
+      content: content,
+      media_type: media_type,
+      media_id: media_id,
+      date: Date.now(),
+    };
+    if (media_type === "tvshow") {
+      const userDocRef = doc(db, "reviews", "tvshow");
+      await setDoc(userDocRef, {
+        uid: uid,
+        displayName: displayName,
+        content: content,
+        media_type: media_type,
+        media_id: media_id,
+        date: Date.now(),
+      });
+    }
+
+    if (media_type === "movie") {
+      const userDocRef = doc(db, "reviews", "movie");
+      await setDoc(userDocRef, {
+        uid: uid,
+        displayName: displayName,
+        content: content,
+        media_type: media_type,
+        media_id: media_id,
+        date: Date.now(),
+      });
+    }
     console.log("User created successfully!");
   } catch (error) {
     console.error("Error creating user:", error);
@@ -39,4 +77,4 @@ async function updateUser(uid, updatedData) {
   }
 }
 
-export {createUserWithEmailAndPassword, updateUser}
+export { createUserWithProfile, updateUser };
