fbae2b9a1de1b880f80bbba8c0a8d2289e9e8dc8
gurman77173 <gurman77173@gmail.com>
2024-09-24 11:30:49 -0600

    better styling for movieDetails page

--- src/components/DetailsPage/MovieDetails.jsx
+++ src/components/DetailsPage/MovieDetails.jsx
@@ -1,10 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useTranslation } from "react-i18next";
+import { RiHeartFill } from "react-icons/ri";
+import { Separator } from "../ui/separator";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "../ui/tooltip";
+import { Button } from "../ui/button";
+import { ArrowRight } from "lucide-react";
+import { Link } from "react-router-dom";
+import { BiPlay } from "react-icons/bi";
+import { BsPauseFill } from "react-icons/bs";
 
 const MovieDetails = ({ id }) => {
+  const videoRef = useRef(null);
   const [movie, setMovie] = useState(null);
   const [videos, setVideos] = useState([]);
   const { t, i18n } = useTranslation();
+  const [isTrailerOn, setIsTrailerOn] = useState(false);
 
 
   useEffect(() => {
@@ -39,67 +49,128 @@ const MovieDetails = ({ id }) => {
         console.error("Error fetching movie videos:", error);
       }
     };
-    
+
     fetchMovieDetails();
     fetchMovieVideos();
   }, [id, i18n.language]);
+
 
   if (!movie) {
     return <div className="min-h-screen flex items-center justify-center text-white">{t('Loading...')}</div>;
   }
 
   return (
-    <div className="max-w-2xl mx-auto p-6 bg-gray-800 rounded-lg shadow-lg text-white">
-      <h1 className="text-3xl font-bold mb-2">{movie.title}</h1>
-      <img 
-        src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`} 
-        alt={movie.title} 
-        className="w-full h-auto rounded-lg mb-4" 
-      />
-      <h2 className="text-xl italic mb-4">{movie.tagline}</h2>
-      <p className="mb-4">{movie.overview}</p>
-      <div className="mb-4">
-        <p><strong>{t('Release Date')}</strong> {new Date(movie.release_date).toLocaleDateString()}</p>
-        <p><strong>{t('Runtime')}</strong> {movie.runtime} {t('minutes')}</p>
-        <p><strong>{t('Genres')}</strong> {movie.genres.map(genre => genre.name).join(', ')}</p>
-        <p><strong>{t('Budget')}</strong> ${movie.budget.toLocaleString()}</p>
-        <p><strong>{t('Revenue')}</strong> ${movie.revenue.toLocaleString()}</p>
-        <p><strong>{t('Vote Average')}</strong> {movie.vote_average} ({movie.vote_count} {t('votes')})</p>
-      </div>
-      <a 
-        href={movie.homepage} 
-        target="_blank" 
-        rel="noopener noreferrer" 
-        className="text-blue-400 underline mt-4 inline-block"
-      >
-        {t('Visit Official Homepage')}
-      </a>
-      
-      {/* Display Movie Trailers */}
-      <div className="mt-8">
-        <h2 className="text-2xl font-bold mb-4">{t('Trailers')}</h2>
+    <div className="mx-auto bg-zinc-900 text-secondary">
+      <div className="relative container p-0  overflow-hidden border border-zinc-700 rounded-md">
+        <div className="relative">
 
-        {videos.length > 0 ? (
-          videos
-            .filter(video => video.site === 'YouTube' && video.type === 'Trailer') // Only YouTube trailers
-            .map(video => (
-              <div key={video.id} className="mb-6">
-                <h3 className="text-xl font-semibold mb-2">{video.name}</h3>
+          <img
+            src={`https://image.tmdb.org/t/p/w1280${movie.backdrop_path}`}
+            alt={movie.title}
+            className="w-full rounded-md mb-4 "
+          />
+
+
+          {videos.length > 0 && isTrailerOn ? (
+            videos
+              .filter(video => video.site === 'YouTube' && video.type === 'Trailer') // Only YouTube trailers
+              .map(video => (
                 <iframe
+                  ref={videoRef}
                   width="100%"
-                  height="315"
-                  src={`https://www.youtube.com/embed/${video.key}`} // Embed YouTube video
+                  src={`https://www.youtube.com/embed/${video.key}?modestbranding=1&controls=0&showinfo=0&listType=playlist&rel=0&autoplay=1`} // Embed YouTube video
                   title={video.name}
                   style={{ border: 'none' }}
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                  allowFullScreen
+                  frameBorder={0}
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  className="absolute top-0 bottom-0 h-full"
                 ></iframe>
-              </div>
-            ))
-        ) : (
-          <p>{t('No trailers available.')}</p>
-        )}
-      </div>      
+
+              ))
+          ) : (
+            <p>{t('No trailers available.')}</p>
+          )}
+
+
+
+
+          <div className="container w-full absolute bottom-0 bg-gradient-to-t to-transparent from-50% from-zinc-900">
+
+
+            <h1 className="text-3xl font-medium">{movie.title}</h1>
+
+            <div className="flex gap-2 my-2">
+
+              {movie.genres.map((genre, index) => {
+                return <div className=" h-7 flex gap-2">
+                  <span className="opacity-70">{genre.name}</span>
+
+                  {movie.genres.length - 1 !== index &&
+                    <Separator orientation='vertical' className="opacity-40" />
+                  }
+                </div>
+              })}
+
+
+            </div>
+
+            <div className="flex gap-3 mt-4">
+
+              <Button className="rounded-full h-auto px-6 m-0 flex gap-1 items-center text-base" asChild>
+
+                <Link to={movie.homepage} target="__blank">
+                  Visit Website <ArrowRight className="size-5" />
+                </Link>
+
+              </Button>
+
+              <TooltipProvider delayDuration={100}>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <button className="hover:border-neutral-300 hover:text-neutral-300 border-2 border-border rounded-full p-2 text-2xl m-0">
+                      <RiHeartFill />
+                    </button>
+                  </TooltipTrigger>
+
+                  <TooltipContent>
+                    <p>Add to favourites</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+
+              <button onClick={() => setIsTrailerOn(prev => !prev)} className="hover:border-neutral-300 hover:text-neutral-300 border-2 border-border rounded-full p-2 text-2xl m-0">
+                {isTrailerOn ?
+                  <BsPauseFill />
+                  :
+                  <BiPlay />
+                }
+              </button>
+            </div>
+
+          </div>
+
+        </div>
+        <h2 className="text-xl italic mb-4">{movie.tagline}</h2>
+        <p className="mb-4">{movie.overview}</p>
+        <div className="mb-4">
+          <p><strong>{t('Release Date')}</strong> {new Date(movie.release_date).toLocaleDateString()}</p>
+          <p><strong>{t('Runtime')}</strong> {movie.runtime} {t('minutes')}</p>
+          <p><strong>{t('Genres')}</strong> </p>
+          <p><strong>{t('Budget')}</strong> ${movie.budget.toLocaleString()}</p>
+          <p><strong>{t('Revenue')}</strong> ${movie.revenue.toLocaleString()}</p>
+          <p><strong>{t('Vote Average')}</strong> {movie.vote_average} ({movie.vote_count} {t('votes')})</p>
+        </div>
+        <a
+          href={movie.homepage}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-400 underline mt-4 inline-block"
+        >
+          {t('Visit Official Homepage')}
+        </a>
+
+
+      </div>
     </div>
   );
 };
