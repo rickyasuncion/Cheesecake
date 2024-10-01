e1d9d701f6e9fcf8a7c011672fe18238bd4fe2fe
willhui1204 <siukee.hui@edu.sait.ca>
2024-10-01 17:16:47 -0600

    ongoing adding chinese

--- src/components/LoginPage/LoginForm.jsx
+++ src/components/LoginPage/LoginForm.jsx
@@ -2,12 +2,14 @@ import React, { useState } from "react";
 import { useUserAuth } from "../../_utils/auth-context";
 
 import { useNavigate } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 const LoginForm = () => {
   const { gitHubSignIn, googleSignIn, emailSignIn } = useUserAuth();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
+  const { t } = useTranslation();
 
   const handleLogin = async (provider) => {
     try {
@@ -26,17 +28,17 @@ const LoginForm = () => {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-[#1c1c1e] text-white">
-      <h1 className="text-4xl font-bold mb-6">Login to Cheesecake</h1>
+      <h1 className="text-4xl font-bold mb-6">{t('Login to Cheesecake')}</h1>
       <input
         type="email"
-        placeholder="Email"
+        placeholder={t('Email')}
         value={email}
         onChange={(e) => setEmail(e.target.value)}
         className="p-2 mb-4 rounded bg-gray-800 text-white placeholder-gray-500 w-80"
       />
       <input
         type="password"
-        placeholder="Password"
+        placeholder={t('Password')}
         value={password}
         onChange={(e) => setPassword(e.target.value)}
         className="p-2 mb-4 rounded bg-gray-800 text-white placeholder-gray-500 w-80"
@@ -47,7 +49,7 @@ const LoginForm = () => {
       >
         Login
       </button>
-      <div className="my-4 text-gray-400">or login with</div>
+      <div className="my-4 text-gray-400">{t('or login with')}</div>
       <div className="flex space-x-4">
         <button
           className="bg-gray-800 text-white p-2 rounded"
@@ -65,7 +67,7 @@ const LoginForm = () => {
       <div className="mt-4 text-gray-400">
         Don't have an account?{" "}
         <button onClick={() => navigate("/signup")} className="text-blue-400">
-          Sign Up
+          {t('Sign Up')}
         </button>
       </div>
     </div>
