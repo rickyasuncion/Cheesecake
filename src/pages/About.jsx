9153e28353403d526ba02bead9dda36aa4fa1dd7
gurman77173 <gurman77173@gmail.com>
2024-10-04 23:04:07 -0600

    made team section in /about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,6 @@
+// Refrences:
+// images taken from: https://unsplash.com
+
 import React from 'react'
 import { Button } from '../components/ui/button'
 import { Link } from 'react-router-dom'
@@ -8,7 +11,8 @@ import { GoHeartFill } from "react-icons/go";
 
 const About = () => {
   return (
-   <div className='relative bg-black isolate text-white'>
+    <div className='container max-w-screen-2xl'>
+   <div className='relative bg-black isolate text-white overflow-hidden rounded-lg mt-3'>
         <img src="/hero.jpg" alt="" className='-z-10 absolute w-full h-full object-cover opacity-20' />
         <div className='py-24 text-center space-y-4'>
             <h1 className='text-5xl font-medium'>The Only place to get movie recommendations</h1>
@@ -26,6 +30,52 @@ const About = () => {
             </div>
         </div>
    </div>
+
+   <div className='py-14'>
+    <h2 className='text-4xl font-medium text-center'>Our dedicated team</h2>
+
+    <div className='flex gap-5 flex-wrap mt-4'>
+    <div className='group flex-1 min-w-80 max-w-md'>
+        <div className='relative bg-[url(https://images.unsplash.com/photo-1522529599102-193c0d76b5b6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)]  bg-cover aspect-square rounded-md grayscale-[50%] group-hover:grayscale-0 transition-[filter]'>
+        <div className='bg-white right-4 left-4 rounded-md absolute bottom-4 p-2'>
+            <p className='font-medium text-lg'>Gordan Willson</p>
+            <span className='text-sm text-accent-foreground font-medium'>Lead developer</span>
+
+        </div>
+        </div>
+    </div>
+    <div className='group flex-1 min-w-80 max-w-md'>
+        <div className='relative bg-[url(https://images.unsplash.com/photo-1489980869433-d1f7c7ac0fcf?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] bg-cover aspect-square rounded-md grayscale-[50%] group-hover:grayscale-0 transition-[filter]'>
+        <div className='bg-white right-4 left-4 rounded-md absolute bottom-4 p-2'>
+            <p className='font-medium text-lg'>Oliver Nacelle </p>
+            <span className='text-sm text-accent-foreground font-medium'>Payments Support</span>
+
+        </div>
+        </div>
+    </div>
+    <div className='group flex-1 min-w-80 max-w-md'>
+        <div className='relative bg-[url(https://images.unsplash.com/photo-1489980721706-f487dab89c24?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] bg-cover aspect-square rounded-md grayscale-[50%] group-hover:grayscale-0 transition-[filter]'>
+        <div className='bg-white right-4 left-4 rounded-md absolute bottom-4 p-2'>
+            <p className='font-medium text-lg'>Oriando Diggs</p>
+            <span className='text-sm text-accent-foreground font-medium'>Cheescake Co-founder</span>
+
+        </div>
+        </div>
+    </div>
+    <div className='group flex-1 min-w-80 max-w-md'>
+        <div className='relative bg-[url(https://images.unsplash.com/photo-1474313438662-85ce389c174a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] bg-cover aspect-square rounded-md grayscale-[50%] group-hover:grayscale-0 transition-[filter]'>
+        <div className='bg-white right-4 left-4 rounded-md absolute bottom-4 p-2'>
+            <p className='font-medium text-lg'>Mohamad Al Sufi</p>
+            <span className='text-sm text-accent-foreground font-medium'>Product co-ordinator</span>
+
+        </div>
+        </div>
+    </div>
+   </div>
+
+    </div>
+
+    </div>
   )
 }
 
