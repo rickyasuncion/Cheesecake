2097cc9e6dba11edda3f4e05f5ae226df8b43e1c
gurman77173 <gurman77173@gmail.com>
2024-10-08 16:50:53 -0600

    successfully fetch similar favourite movies

--- src/pages/SimilarLiked.jsx
+++ src/pages/SimilarLiked.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { fetchAndSetFavouriteMovies, fetchAndSetFavouriteTv } from '../_utils/utils';
 
 const SimilarLiked = () => {
+    const [favouriteMovies, setFavouritesMovies] = useState([]);
+    const [favouriteTv, setFavouriteTv] = useState([]);
+    const [similarFavMovies, setSimilarFavMovies] = useState({});
+
+    useEffect(() => {
+        fetchAndSetFavouriteMovies(setFavouritesMovies)
+        fetchAndSetFavouriteTv(setFavouriteTv)
+
+    }, [])
+
+    useEffect(() => {
+        favouriteMovies.map(async movie => {
+            const res = await fetch(`https://api.themoviedb.org/3/movie/${movie.id}/similar?language=en-US&page=1&api_key=bbd89781c7835917a2decb4989b56470`);
+            const relatedMovies = await res.json();
+
+            setSimilarFavMovies(prev => {
+                const temp = { ...prev };
+                temp[movie.title] = relatedMovies.results
+                return temp;
+            })
+        })
+    }, [favouriteMovies.length])
+
+
     return (
-        <div>SimilarLiked</div>
+        <div>
+            <h2 className='text-lg font-medium underline'>Similar favourite movies</h2>
+
+        </div>
     )
 }
 
