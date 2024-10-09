b834c68e45e288b9dba4ff55e7cb66b8192233d9
gurman77173 <gurman77173@gmail.com>
2024-10-09 12:48:10 -0600

    made recommendation page to have tvShows

--- src/pages/SimilarLiked.jsx
+++ src/pages/SimilarLiked.jsx
@@ -8,12 +8,27 @@ const SimilarLiked = () => {
     const [favouriteMovies, setFavouritesMovies] = useState([]);
     const [favouriteTv, setFavouriteTv] = useState([]);
     const [similarFavMovies, setSimilarFavMovies] = useState({});
+    const [similarFavTvShows, setSimilarFavTvShows] = useState({});
 
     useEffect(() => {
         fetchAndSetFavouriteMovies(setFavouritesMovies)
         fetchAndSetFavouriteTv(setFavouriteTv)
 
     }, [])
+
+
+    useEffect(() => {
+        favouriteTv.map(async tvShow => {
+            const res = await fetch(`https://api.themoviedb.org/3/tv/${tvShow.id}/similar?language=en-US&page=1&api_key=bbd89781c7835917a2decb4989b56470`);
+            const relatedTvShows = await res.json();
+
+            setSimilarFavTvShows(prev => {
+                const temp = { ...prev };
+                temp[tvShow.name] = relatedTvShows.results
+                return temp;
+            })
+        })
+    }, [favouriteTv.length])
 
     useEffect(() => {
         favouriteMovies.map(async movie => {
@@ -36,6 +51,15 @@ const SimilarLiked = () => {
                 {Object.keys(similarFavMovies).map(key => {
                     return similarFavMovies[key].length === 0 ? <div className='container'> <h2 className="text-lg font-medium">{t(key)}</h2><h4>No Similar movies found</h4></div> :
                         <MovieListView movies={similarFavMovies[key].map(movie => ({ ...movie, media_type: 'movie' }))} title={t(key)} contentType='movies' />
+                })}
+            </div>
+
+
+            <h2 className='text-lg font-medium underline'>Similar favourite TV shows</h2>
+            <div className='space-y-4 py-2'>
+                {Object.keys(similarFavTvShows).map(key => {
+                    return similarFavTvShows[key].length === 0 ? <div className='container'> <h2 className="text-lg font-medium">{t(key)}</h2><h4>No Similar shows found</h4></div> :
+                        <MovieListView movies={similarFavTvShows[key].map(tvShow => ({ ...tvShow, media_type: 'tv' }))} title={t(key)} contentType='tv-shows' />
                 })}
             </div>
         </div>
