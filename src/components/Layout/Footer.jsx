1a422740d542c5ab87acd77b7a659b79370f82e9
DaljotKaur <daljotkaur@edu.sait.ca>
2024-10-23 22:58:04 -0600

    settings page

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -1,42 +1,43 @@
-
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { Button } from '../ui/button'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from '../ui/button';
+import { useTranslation } from 'react-i18next';
 
 const Footer = () => {
+  const { t } = useTranslation();
+
   return (
-    <footer class="bg-black text-white">
-      <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-        <div class="sm:flex sm:items-center sm:justify-between">
-          <Link to={'/'} class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-            <span class="self-center text-2xl font-semibold whitespace-nowrap ">CheeseCake</span>
+    <footer className="bg-black text-white">
+      <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <Link to={'/'} className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+            <span className="self-center text-2xl font-semibold whitespace-nowrap">CheeseCake</span>
           </Link>
-          <ul class="flex flex-wrap items-center mb-6 text-sm font-medium sm:mb-0">
+          <ul className="flex flex-wrap items-center mb-6 text-sm font-medium sm:mb-0">
             <li>
-              <Link to={'/'} class="hover:underline me-4 md:me-6">Home</Link>
+              <Link to={'/'} className="hover:underline me-4 md:me-6">{t('Home')}</Link>
             </li>
             <li>
-              <Link to={'/about'} class="hover:underline me-4 md:me-6">About</Link>
+              <Link to={'/about'} className="hover:underline me-4 md:me-6">{t('About')}</Link>
             </li>
             <li>
-              <Link to={'/movies'} class="hover:underline me-4 md:me-6">Movies</Link>
+              <Link to={'/movies'} className="hover:underline me-4 md:me-6">{t('Movies')}</Link>
             </li>
             <li>
-              <Link to={"/tvShows"} class="hover:underline me-4 md:me-6">TV Shows</Link>
+              <Link to={"/tvShows"} className="hover:underline me-4 md:me-6">{t('TV Shows')}</Link>
             </li>
             <li>
               <Button asChild className="bg-red-500 text-white hover:bg-red-800">
-                <Link to="/favourites" class="hover:underline">Favourites</Link>
+                <Link to="/favourites" className="hover:underline">{t('Favourites')}</Link>
               </Button>
             </li>
           </ul>
         </div>
-        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span class="block text-sm text-gray-300 sm:text-center ">© 2024 <Link to={'/'} class="hover:underline">Cheescake</Link>. All Rights Reserved.</span>
+        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span className="block text-sm text-gray-300 sm:text-center">© 2024 <Link to={'/'} className="hover:underline">Cheesecake</Link>. {t('All Rights Reserved')}</span>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
-
+export default Footer;
