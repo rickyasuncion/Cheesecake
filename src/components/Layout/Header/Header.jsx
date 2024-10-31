7dab04a3e1ffdb19396e1a08d3dc79ac47ac2a1b
gurman77173 <gurman77173@gmail.com>
2024-10-31 16:14:01 -0600

    added tooltip to toggle button for better ux

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -7,6 +7,7 @@ import { FaCirclePlay } from "react-icons/fa6";
 
 import { Toggle } from "../../ui/toggle";
 import { useMovieTrailerContext } from "../../../providers/MovieTrailerProvider";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "../../ui/tooltip";
 
 const Header = () => {
   const { toggleTrailerState } = useMovieTrailerContext();
@@ -16,9 +17,29 @@ const Header = () => {
       <header className="py-3 container flex justify-between items-center">
         <NavBar />
 
-        <Toggle className="text-white" onPressedChange={toggleTrailerState} >
-          <FaCirclePlay className="size-6" />
-        </Toggle>
+
+        <TooltipProvider>
+          <Tooltip>
+
+            <TooltipTrigger>
+
+              <Toggle className="text-white" onPressedChange={toggleTrailerState} >
+                <FaCirclePlay className="size-6" />
+              </Toggle>
+
+            </TooltipTrigger>
+
+            <TooltipContent>
+
+              <p>Should play trailers on hover?</p>
+            </TooltipContent>
+
+          </Tooltip>
+
+        </TooltipProvider>
+
+
+
         <div className="header-right flex items-center gap-3">
           <SearchBar />
           <LanguageSelector />
