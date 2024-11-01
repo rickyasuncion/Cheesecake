ce175f602270feda4129b53c5d6b9cb6348238a1
rickyasuncion <rickyasuncion@outlook.com>
2024-11-01 09:30:01 -0600

    complete searchbar

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -77,16 +77,7 @@ const Header = () => {
         </Tooltip>
       </TooltipProvider>
       <div className="flex items-center space-x-4">
-        <div className="relative text-white dark:text-black">
-          <input
-            type="search"
-            // value={searchTerm}
-            // onChange={(e) => setSearchTerm(e.target.value)}
-            // placeholder={t("Search...")}
-            className="bg-gray-800 rounded-full px-3 py-1 pl-8 text-sm focus:outline-none focus:ring-2 focus:ring-yellow-400 w-40"
-          />
-          <Search className="absolute left-2 top-1.5 h-4 w-4 text-gray-400" />
-        </div>
+        <SearchBar/>
         <LanguageSelector />
 
         <Menu className="h-5 w-5" />
