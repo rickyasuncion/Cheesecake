b33d8fb9f02f2deea5d39eecafd9690ca2788fb2
rickyasuncion <rickyasuncion@outlook.com>
2024-11-08 18:19:01 -0700

    small tweaks
    movie page mostly complete, however tvshows require considerably more functions.

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -109,7 +109,7 @@ const Details = () => {
             <DetailsInfo overview={movie.overview} cast={cast} />
             {/* <Reviews /> */}
           </div>
-          <Sidebar movie={movie} crews={crew} type={type}/>
+          <Sidebar movie={movie} crews={crew} />
         </div>
         <Similiar similarMovies={similiar} />
       </div>
